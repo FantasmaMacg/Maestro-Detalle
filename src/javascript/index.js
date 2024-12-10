4cb18bb86c4437b532078bf848e17a712df54650
MACG <curso08@nomail.com>
2024-12-10 14:00:52 +0100

    acabado

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,65 +1,77 @@
-$(document).ready(function() {
- 
-    $('#Primero').click(function() {
+
+$(document).ready(function () {
+
+    $('#Primero').click(function () {
         $.get("https://my-json-server.typicode.com/typicode/demo/posts",
-        function(data) {
-            $('#1').val('Bien'); 
-        }).fail(function() {
-            $('#1').val('Error al realizar GET');
-        });
+            function (data) {
+                $('#1').val('Bien');
+            }).fail(function () {
+                $('#1').val('Error al realizar GET');
+            });
     });
 
-    
-    $('#Segundo').click(function() {
-        $.post("https://my-json-server.typicode.com/typicode/demo/posts", 
-        function(data) {
-            $('#2').val('Bien'); 
-        }).fail(function() {
-            $('#2').val('Error al realizar POST');
-        });
+
+    $('#Segundo').click(function () {
+        $.post("https://my-json-server.typicode.com/typicode/demo/posts",
+            function (data) {
+                $('#2').val('Bien');
+            }).fail(function () {
+                $('#2').val('Error al realizar POST');
+            });
     });
 
-    
-    $('#Tercero').click(function() {
+
+    $('#Tercero').click(function () {
         $.ajax({
             url: "https://my-json-server.typicode.com/typicode/demo/posts/1",
             type: "PUT",
-          
-            success: function(data) {
+
+            success: function (data) {
                 $('#3').val('Bien'); o
             },
-            error: function() {
+            error: function () {
                 $('#3').val('Error al realizar PUT');
             }
         });
     });
 
-   
-    $('#Cuarto').click(function() {
+
+    $('#Cuarto').click(function () {
+        
+        $(this).addClass('mio')
         $.ajax({
             url: "https://my-json-server.typicode.com/typicode/demo/posts/1",
             type: "PATCH",
-           
-            success: function(data) {
-                $('#4').val('Bien'); 
+
+            success: function (data) {
+                $('#4').val('Bien');
             },
-            error: function() {
+            error: function () {
                 $('#4').val('Error al realizar PATCH');
             }
         });
     });
 
-   
-    $('#Cinco').click(function() {
+
+
+
+
+    $('#Cinco').click(function () {
         $.ajax({
             url: "https://my-json-server.typicode.com/typicode/demo/posts/1",
             type: "DELETE",
-            success: function() {
+            success: function () {
+
                 $('#5').val('Eliminado correctamente');
             },
-            error: function() {
+            error: function () {
                 $('#5').val('Error al realizar DELETE');
             }
         });
+
     });
+
+
+
 });
+
